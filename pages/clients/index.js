322e2acd0e5a16a8bf29342e9ba3feac511269b4
javierpb275 <javierpb275@gmail.com>
2021-09-02 18:26:43 +0200

    Navigating to Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,34 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+    const clients = [
+        {
+            id: 'max',
+            name: 'Max'
+        },
+        {
+            id: 'pepe',
+            name: 'Pepe'
+        },
+    ]
     return (
         <div>
             <h1>The Clients Page</h1>
+            <ul>
+                {
+                    clients.map(client => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    </li>
+                ))
+                }
+{/*                 <li>
+                    <Link href="/clients/max">Max</Link>
+                </li>
+                <li>
+                    <Link href="/clients/pepe">Pepe</Link>
+                </li> */}
+            </ul>
         </div>
     );
 }
