3047aa8e1f290b61a3c24fd5ee7550c550379f02
javierpb275 <javierpb275@gmail.com>
2021-09-02 18:03:40 +0200

    Adding Catch-All Routes: - created blog/[...slug].js - we can access the page by doing for example: /blog/2021/09 - router.query: slug: (2) ['2021', '09'] - we get an array with 2021 and 09 that we passed in the route.

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,13 @@
+import {useRouter} from 'next/router';
+
+const BlogPostsPage = () => {
+    const router = useRouter();
+    console.log(router.query);
+    return (
+        <div>
+            <h1>The blog posts</h1>
+        </div>
+    );
+}
+
+export default BlogPostsPage;
